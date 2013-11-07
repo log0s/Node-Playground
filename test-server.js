d7e9f8c803ef15583b9a9f9cba7e107d5516f347
log0s <JewishRhino@gmail.com>
2013-11-07 15:31:32 -0700

    Playing around with server stuff

--- test-server.js
+++ test-server.js
@@ -1,7 +1,11 @@
-var http = require('http');
+var http = require('http'),
+    onRequest = function(request, response) {
+        console.log('Request received.');
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('Hello World');
+        response.end();
+    };
 
-http.createServer(function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
-    response.end();
-}).listen(8888);
+http.createServer(onRequest).listen(8888);
+
+console.log('Server started');
